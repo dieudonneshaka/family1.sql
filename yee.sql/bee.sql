77dd33d36a36a0e10e68723778200a184b037234
dieudonneshaka5 <dieudonneshaka5@gmail.com>
2024-12-20 13:17:05 +0200

    reademe

--- yee.sql/bee.sql
+++ yee.sql/bee.sql
@@ -1,9 +1,6 @@
-# family1.sql
-
 family1
 database name is 123
 Use SELECT * from to call tables from database.
 
 The table will store information about family members.
 including their relationships to one and another.
-
