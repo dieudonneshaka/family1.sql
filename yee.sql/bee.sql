43667ea2dff0484edfd2440e547b99ced7cf3c1c
dieudonneshaka5 <dieudonneshaka5@gmail.com>
2024-12-20 13:22:07 +0200

    reame file

--- yee.sql/bee.sql
+++
@@ -1,6 +0,0 @@
-family1
-database name is 123
-Use SELECT * from to call tables from database.
-
-The table will store information about family members.
-including their relationships to one and another.
